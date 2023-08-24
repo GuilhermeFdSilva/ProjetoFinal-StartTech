e2b23aa84fa699fae13a9f8299911edef9130e5f
Guilherme França da Silva <francaguilherme27@gmail.com>
2023-08-24 15:24:29 -0300

    Criação da rota de cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { HomeComponent } from './home/home.component';
 import { QuemSomosComponent } from './quem-somos/quem-somos.component';
 import { DetalhesComponent } from './detalhes/detalhes.component';
 import { PerfilClienteComponent } from './perfil-cliente/perfil-cliente.component';
+import { CadastroClienteComponent } from './cadastro-cliente/cadastro-cliente.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'perfil-cliente', component: PerfilClienteComponent },
+  { path: 'cadastro', component: CadastroClienteComponent },
   { path: 'quem-somos', component: QuemSomosComponent },
   { path: 'detalhes/:id', component: DetalhesComponent },
 
