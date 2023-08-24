e91bb90af820a14489b94fc0a0f47fefc3b3a386
Guilherme <francaguilherme27@gmail.com>
2023-08-23 21:23:29 -0300

    Atualizando footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  links: Array<any> = ['pedra', 'papel', 'tesoura'];
+  redesSociais: Array<any> = [{ imagem: '123', descricao: 'Facebook', link: '123' }]
 }
