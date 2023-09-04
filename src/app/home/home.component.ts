1955fba508079a8b8ed2bd29244a9ca1e75517b0
Guilherme <francaguilherme27@gmail.com>
2023-09-04 00:12:41 -0300

    Correção para deploy

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
+import { UsuarioService } from 'src/assets/services/usuario/usuario.service';
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { ProdutoService } from 'src/assets/services/produto/produto.service';
 
@@ -12,9 +12,16 @@ export class HomeComponent implements OnInit {
   itens: any[] = [];
   itensFiltrados: any[] = [];
   usuarios: any[] = [];
+  tag: string = '';
 
-  constructor(private http: HttpClient, private router: Router, protected produtoService: ProdutoService) {
-    this.itensFiltrados = [...this.itens];
+  constructor(private router: Router, protected produtoService: ProdutoService, protected usuarioService: UsuarioService) {
+    this.usuarioService.atualizarDados.subscribe((response) => {
+      if (response === 'Item') {
+        this.usuarios = this.usuarioService.getUsuarios();
+        this.itens = produtoService.getItens();
+        this.itensFiltrados = [...this.itens];
+      }
+    });
 
     window.addEventListener('scroll', () => {
       if (window.scrollY > 66) {
@@ -26,21 +33,19 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.http.get<any>('./assets/dummy.json').subscribe((dummy) => {
-      this.itens = dummy.itens;
-      this.usuarios = dummy.usuarios;
-      this.itensFiltrados = [...this.itens];
-      // O operador ..., chamado operador de propagação ou spread operator em
-      // JavaScript/TypeScript, é usado para criar cópias de arrays e objetos,
-      // bem como para espalhar os elementos de um array ou objeto em outro.
-    });
+    this.usuarios = this.usuarioService.getUsuarios();
+    this.itens = this.produtoService.getItens();
+    this.itensFiltrados = [...this.itens];
+    // O operador ..., chamado operador de propagação ou spread operator em
+    // JavaScript/TypeScript, é usado para criar cópias de arrays e objetos,
+    // bem como para espalhar os elementos de um array ou objeto em outro.
   }
 
   getNomeUsuario(usuarioId: number): string {
     const usuario = this.usuarios.find((u) => u.id === usuarioId);
     return usuario ? usuario.nome : 'Desconhecido';
   }
-  
+
 
   verDetalhes(itemId: number) {
     this.router.navigate(['detalhes', itemId]);
@@ -57,12 +62,6 @@ export class HomeComponent implements OnInit {
   }
 
   filtrarItensPorTag(tag: string) {
-    if (tag === '') {
-      this.itensFiltrados = this.itens; // Mostrar todos os itens
-    } else {
-      this.itensFiltrados = this.itens.filter((item) =>
-        item.tags.includes(tag)
-      );
-    }
+    this.tag = tag;
   }
 }
