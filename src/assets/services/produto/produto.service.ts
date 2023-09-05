72f2cdb8714ec21d419e908c5befa1beb96a1caf
Guilherme <francaguilherme27@gmail.com>
2023-09-05 08:45:01 -0300

    Finalização

--- src/assets/services/produto/produto.service.ts
+++ src/assets/services/produto/produto.service.ts
@@ -133,15 +133,15 @@ export class ProdutoService {
   }
 
   private getDados(): Observable<any> {
-    return this.http.get<any>('http://localhost:3000/itens');
+    return this.http.get<any>('https://json-server-antiquary.vercel.app/itens');
   }
 
   private addDados(novoItem: Produto): Observable<any> {
-    return this.http.post('http://localhost:3000/itens', novoItem);
+    return this.http.post('https://json-server-antiquary.vercel.app/itens', novoItem);
   }
 
   private atualizarItem(itemAtualizado: Produto): Observable<any> {
-    return this.http.patch(`http://localhost:3000/itens/${this.item.getId()}`,
+    return this.http.patch(`https://json-server-antiquary.vercel.app/itens/${this.item.getId()}`,
       {
         titulo: itemAtualizado.getTitulo(),
         descricao: itemAtualizado.getDescricao(),
@@ -152,7 +152,7 @@ export class ProdutoService {
   }
 
   private deletarItem(itemId: number): Observable<any> {
-    return this.http.delete(`http://localhost:3000/itens/${itemId}`);
+    return this.http.delete(`https://json-server-antiquary.vercel.app/itens/${itemId}`);
   }
 
   constructor(private http: HttpClient, private usuarioService: UsuarioService) {
